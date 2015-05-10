04ff8cf3decbdb3b0d2391b5bbc3715d8b70ba1c
yuan xin <crossin@sy.lan>
2015-05-10 18:30:05 +0800

    L1.在网页上显示信息

--- code.py
+++ code.py
@@ -1,12 +1,25 @@
 import web
 
+render = web.template.render('templates/')
+
 urls = (
     '/', 'index'
 )
 
+movies = [
+    {
+        'title': 'Forrest Gump',
+        'year': 1994,
+    },
+    {
+        'title': 'Titanic',
+        'year': 1997,
+    },
+]
+
 class index:
     def GET(self):
-        return "Hello, world!"
+       return render.index(movies)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
