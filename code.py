a38885622b7753fa57281cd9e8a64320739aeaf3
yuan xin <crossin@sy.lan>
2015-05-10 18:32:02 +0800

    L3.更多的页面

--- code.py
+++ code.py
@@ -4,13 +4,21 @@ db = web.database(dbn='sqlite', db='MovieSite.db')
 render = web.template.render('templates/')
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/movie/(\d+)', 'movie',
 )
+
 
 class index:
     def GET(self):
         movies = db.select('movie')
         return render.index(movies)
+
+class movie:
+    def GET(self, movie_id):
+        movie = db.select('movie', where='id=$int(movie_id)', vars=locals())[0]
+        return render.movie(movie)
+
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
