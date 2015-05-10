6a1b8d7789a0b2c65e48cfb86da9f85e556d362a
yuan xin <crossin@sy.lan>
2015-05-10 18:31:06 +0800

    L2.简单的数据库

--- code.py
+++ code.py
@@ -1,25 +1,16 @@
 import web
 
+db = web.database(dbn='sqlite', db='MovieSite.db')
 render = web.template.render('templates/')
 
 urls = (
     '/', 'index'
 )
 
-movies = [
-    {
-        'title': 'Forrest Gump',
-        'year': 1994,
-    },
-    {
-        'title': 'Titanic',
-        'year': 1997,
-    },
-]
-
 class index:
     def GET(self):
-       return render.index(movies)
+        movies = db.select('movie')
+        return render.index(movies)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
